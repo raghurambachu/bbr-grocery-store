03e6991091d9e613e33df1f9eef7eebc96cd91d6
Raghuram <1993raghuram@gmail.com>
2020-12-20 09:59:54 +0530

    Added Customer and OrderItem - responsible for calculating order amount using polymorphism

--- lib/price_calculator.rb
+++ lib/price_calculator.rb
@@ -1,8 +1,9 @@
 require_relative("grocery_store")
 require_relative("product")
+require_relative("order_item")
 require_relative("customer")
 
-grocery_store = GroceryStore.new("Big Bazaar")
+store = GroceryStore.new("Big Bazaar")
 
 product1 = Product.new("milk",3.97,2,5)
 product2 = Product.new("bread",2.17,3,6)
@@ -10,22 +11,21 @@ product3 = Product.new("banana",0.99)
 product4 = Product.new("apple",0.89)
 
 
-grocery_store.add_product(product1)
-grocery_store.add_product(product2)
-grocery_store.add_product(product3)
-grocery_store.add_product(product4)
+store.add_product(product1)
+store.add_product(product2)
+store.add_product(product3)
+store.add_product(product4)
 
-GroceryStore.pricetable(grocery_store)
+GroceryStore.pricetable(store)
 puts 
-
-
-
-
 
 # is an Hash containing order details ie order_item => count
 customer_order = GroceryStore.take_customer_input
 
-print customer_order
+customer1 = Customer.new(customer_order,store)
+customer1.generate_item
+
+
 
 
 
